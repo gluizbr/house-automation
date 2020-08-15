8de93aca2b92161f4c8024b1dd824775c01b0670
Alessandro Paulichen <paulichen.ale@gmail.com>
2020-08-15 18:55:44 -0300

    Create modules

--- house-automation-frontend/src/app/app-routing.module.ts
+++ house-automation-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./central/central.module').then(m => m.CentralModule)
+  },
+  {
+    path: 'sensores',
+    loadChildren: () => import('./sensores/sensores.module').then(m => m.SensoresModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
